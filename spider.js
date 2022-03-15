5932aea9bf1126bbedded87586c503bb4c1ce401
OSH <llll_lll_lll@naver.com>
2022-03-16 00:25:52 +0900

    함수 모듈화

--- spider.js
+++ spider.js
@@ -7,25 +7,33 @@ const { urlToFilename } = require('./utils.js');
 function spider(url, cb) {
   const filename = urlToFilename(url);
   fs.access(filename, (err) => {
-    if (err && err.code === 'ENOENT') {
-      console.log(`Downloading ${url} into ${filename}`);
+    if (!err || err.code !== 'ENOENT') return cb(null, filename, false);
+    
+    download(url, filename, err => {
+      if (err) return cb(err);
+      cb(null, filename, true);
+    })
+  });
+}
+
+function download(url, filename, cb) {
+  console.log(`Downloading ${url} into ${filename}`);
       superagent.get(url).end((err, res) => {
-        if (err) {
-          return cb(err);
-        }
-        mkdirp(path.dirname(filename), (err) => {
-          if (err) {
-            return cb(err);
-          }
-          fs.writeFile(filename, res.text, (err) => {
-            if (err) {
-              return cb(err);
-            }
-            cb(null, filename, true);
-          });
-        });
+        if (err) return cb(err);
+        
+        saveFile(filename, res.text, cb);
       });
-    }
+}
+
+function saveFile(filename, contents, cb) {
+  mkdirp(path.dirname(filename), (err) => {
+    if (err) return cb(err);
+    
+    fs.writeFile(filename, contents, (err) => {
+      if (err) return cb(err);
+      
+      cb(null, filename, true);
+    });
   });
 }
 
