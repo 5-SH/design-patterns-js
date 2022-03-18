a20ef5fd93ba3198e3eff50e60155174571898b8
OSH <llll_lll_lll@naver.com>
2022-03-18 13:44:51 +0900

    프로미스를 활용한 순차 비동기 제어 흐름

---
+++ async-control-with-promise-async-await/sequential/spider.js
@@ -0,0 +1,50 @@
+const { promises: fsPromises } = require('fs');
+const { dirname } = require('path');
+const superagent = require('superagent');
+const mkdirp = require('mkdirp');
+const { urlToFilename, getPageLinks } = require('../utils');
+const { promisify } = require('util');
+
+const mkdirPromises = promisify(mkdirp);
+
+function download(url, filename) {
+  console.log(`Downloading ${url}`);
+  let content;
+  return superagent.get(url)
+    .then(res => {
+      content = res.text;
+      return mkdirPromises(dirname(filename));
+    })
+    .then(() => fsPromises.writeFile(filename, content))
+    .then(() => {
+      console.log(`Downloaded and saved: ${url}`)
+      return content;
+    });
+}
+
+function spiderLinks(currentUrl, content, nesting) {
+  let promise = Promise.resolve();
+  if (nesting === 0) return promise;
+
+  const links = getPageLinks(currentUrl, content);
+  for (const link of links) {
+    promise = promise.then(() => spider(link, nesting - 1));
+  }
+
+  return promise;
+}
+
+function spider(url, nesting) {
+  const filename = urlToFilename(url);
+  return fsPromises.readFile(filename, 'utf-8')
+    .catch(err => {
+      if (err.code !== 'ENOENT') throw err;
+
+      return download(url, filename);
+    })
+    .then(content => spiderLinks(url, content, nesting));
+}
+
+module.exports = {
+  spider
+}
