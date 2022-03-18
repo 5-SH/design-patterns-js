fc62da197d4bbf9542f9458ecfbff052237ebeb9
OSH <llll_lll_lll@naver.com>
2022-03-18 16:37:03 +0900

    제한된 병렬 실행을 async, await 로 수정

--- async-control-with-promise-async-await/queue/spider.js
+++ async-control-with-promise-async-await/queue/spider.js
@@ -32,6 +32,8 @@ function spiderLinks(currentUrl, content, nesting, queue) {
   let promise = Promise.resolve();
   if (nesting === 0) return promise;
 
+  const links = getPageLinks(currentUrl, content);
+  
   const promises = links.map(link => spiderTask(link, nesting - 1, queue));
   return Promise.all(promises);
 }
