79693df00c940c7768a527687af305f66451915e
OSH <llll_lll_lll@naver.com>
2022-03-19 06:58:54 +0900

    생산자 소비자 패턴으로 제한된 병렬 실행

---
+++ async-control-with-promise-async-await/producer-consumer/spider.js
@@ -0,0 +1,57 @@
+const { promises: fsPromises } = require('fs');
+const { dirname } = require('path');
+const superagent = require('superagent');
+const mkdirp = require('mkdirp');
+const { urlToFilename, getPageLinks } = require('../utils');
+const { promisify } = require('util');
+const TaskQueuePC = require('./taskQueuePC');
+
+const mkdirPromises = promisify(mkdirp);
+
+async function download(url, filename) {
+  console.log(`Downloading ${url}`);
+  const { text: content } = await superagent.get(url);
+  await mkdirPromises(dirname(filename));
+  await fsPromises.writeFile(filename, content);
+  console.log(`Downloaded and saved: ${url}`);
+  return content;
+}
+
+function spiderLinks(currentUrl, content, nesting, queue) {
+  let promise = Promise.resolve();
+  if (nesting === 0) return promise;
+
+  const links = getPageLinks(currentUrl, content);
+  
+  const promises = links.map(link => spiderTask(link, nesting - 1, queue));
+  return Promise.all(promises);
+}
+
+const spidering = new Set();
+function spiderTask(url, nesting, queue) {
+  if (spidering.has(url)) {
+    return Promise.resolve();
+  }
+  spidering.add(url);
+
+  const filename = urlToFilename(url);
+
+  return queue.runTask(() => {
+    return fsPromises.readFile(filename, 'utf-8')
+      .catch(err => {
+        if (err.code !== 'ENOENT') throw err;
+
+        return download(url, filename);
+      })
+      .then(content => spiderLinks(url, content, nesting, queue));
+  });
+}
+
+function spider(url, nesting, concurrency) {
+  const queue = new TaskQueuePC(concurrency);
+  return spiderTask(url, nesting, queue);
+}
+
+module.exports = {
+  spider
+}
