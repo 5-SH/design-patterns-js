f06c727e910a005d993d25ffa506bdbe0aff2385
OSH <llll_lll_lll@naver.com>
2022-03-28 18:27:56 +0900

    순서가 없는 병렬 실행 추가

---
+++ stream-coding/unordered-parallel-execution/check-url.js
@@ -0,0 +1,32 @@
+const { createReadStream, createWriteStream } = require('fs');
+const split = require('split');
+const { pipeline } = require('stream');
+const superagent = require('superagent');
+const { ParallelStream } = require('./parallel-stream.js');
+
+pipeline(
+  createReadStream(process.argv[2]),
+  split(),
+  new ParallelStream(
+    async (url, enc, push, done) => {
+      if (!url) {
+        return done();
+      }
+      try {
+        await superagent.head(url, { timeout: 5 * 1000 });
+        push(`${url} is up\n`);
+      } catch (err) {
+        push(`${url} is down\n`);
+      }
+      done();
+    }
+  ),
+  createWriteStream('results.txt'),
+  err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log('All urls have been checked');
+  }
+);
