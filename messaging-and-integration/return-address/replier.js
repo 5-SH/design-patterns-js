cb40da440f47194be418452973421d06a92db592
OSH <llll_lll_lll@naver.com>
2022-03-31 23:36:46 +0900

    amqp 와 반환 주소로 요청, 응답 패턴 구현

---
+++ messaging-and-integration/return-address/replier.js
@@ -0,0 +1,13 @@
+const AMQPReply = require('./amqpReply.js');
+
+async function main() {
+  const reply = new AMQPReply('requests_queue');
+  await reply.initialize();
+
+  reply.handleRequests(req => {
+    console.log('Request received', req);
+    return { sum: req.a + req.b };
+  });
+}
+
+main().catch(err => console.error(err));
